6c8e5ff997bc96cdc1c73cccc34d7f75cb85c0c9
nitinnavik <naviknitin143@gmail.com>
2022-11-10 15:34:45 +0530

    changes.

--- src/components/business_cards.js
+++ src/components/business_cards.js
@@ -3,60 +3,7 @@ import React, {useEffect, useState} from 'react';
 
 import {SmallCardBusiness} from './small_cards_business';
 import axios from 'react-native-axios';
-import {fetchBusiness} from '../API/api';
 
-const DATA = [
-  {
-    id: 1,
-    title: 'First Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description:
-      'Service available for or business, leisure, or medical reasons -Taxi service with experienced drive',
-    src: '../assets/photo.jpg',
-  },
-  {
-    id: 2,
-    title: 'Second Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description:
-      'HOME TUITION IN THRISSUR- ICSE, CBSE, STATE BOARD, NIOS- ALL SUBJECTS',
-    src: '../assets/photo.jpg',
-  },
-  {
-    id: 3,
-    title: 'Third Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description: 'House of Highgine, Mask and other Highgine Products',
-    src: '../assets/photo.jpg',
-  },
-  {
-    id: 4,
-    title: 'Fourth Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description: 'House of Highgine, Mask and other Highgine Products',
-    src: '../assets/photo.jpg',
-  },
-  {
-    id: 5,
-    title: 'Fifth Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description: 'House of Highgine, Mask and other Highgine Products',
-    src: '../assets/photo.jpg',
-  },
-  {
-    id: 6,
-    title: 'Sixth Item',
-    category: 'Tours/Travels',
-    area: 'Vennala',
-    description: 'House of Highgine, Mask and other Highgine Products',
-    src: '../assets/photo.jpg',
-  },
-];
 const BusinessCards = () => {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
